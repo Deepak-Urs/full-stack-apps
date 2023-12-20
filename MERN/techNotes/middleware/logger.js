29ff172d8bc434248404796c492def492687b666
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-12-20 14:45:50 -0800

    added customHandlers: logger and errorhandler

--- MERN/techNotes/middleware/logger.js
+++ MERN/techNotes/middleware/logger.js
@@ -1,10 +1,11 @@
-const { format } = reqyure('date-fns')
+const { format } = require('date-fns')
 const {v4: uuid} = require('uuid')
 const fs = require('fs')
 const fsPromises = require('fs').promises
 const path = require('path')
 const { dirname } = require('path')
 
+//logging events
 const logEvents = async (message, logFileName) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`
@@ -18,3 +19,12 @@ const logEvents = async (message, logFileName) => {
         console.log(err);
     }
 }
+
+//actual middleware
+const logger = (req, res, next) => {
+    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log')
+    console.log(`${req.method} ${req.path}`);
+    next()
+}
+
+module.exports = {logEvents, logger}
--- MERN/techNotes/server.js
+++ MERN/techNotes/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 3500
 
+app.use(logger)
 app.use(express.json())
 
 app.use('/', express.static(path.join(__dirname, 'public')))
@@ -12,13 +15,14 @@ app.use('/', require('./routes/root'))
 
 app.all('*', (req, res) => {
     res.status(400)
-    if(req.accepts('html')) {
+    if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'))
     } else if (req.accepts('json')) {
-        res.json({message: "404 Not found"})
+        res.json({ message: "404 Not found" })
     } else {
         res.type('text').send('404 Not Found!')
     }
 })
 
+app.use(errorHandler)
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
